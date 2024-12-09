90c84b061c020a212c9da1fa9f8a7cf3f5223f1f
adrijimdev <adrianjimenez1994@gmail.com>
2024-12-09 12:07:35 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { NotesListComponent } from './components/notes-list/notes-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'notes-list', component: NotesListComponent},
+  { path: '', redirectTo: '/notes-list', pathMatch: 'full' }
+];
---
+++ src/app/components/notes-list/notes-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'notes-list',
+  imports: [],
+  templateUrl: './notes-list.component.html',
+  styleUrl: './notes-list.component.css'
+})
+export class NotesListComponent {
+  ngOnInit() {
+    console.log("holaquetal")
+  }
+}
